fbcf9dfd0cd97b14df53828ddaaa8a6bb15c2d66
trungnq1 <trungnq1@smartosc.com>
2015-11-09 11:25:34 +0700

    update fix bug

--- js/main.js
+++ js/main.js
@@ -124,16 +124,14 @@ $(document).ready(function(){
     	if($(this).hasClass('more')){
     		$(this).parent('.button-read').siblings('.expander').find('.more-link').click();
     		$(this).text("Read less");
-    		if ($(window).width() >= 1900) {
-    			$(this).parents().find(".block-toggle").css("background-size","60% 100%");
-    		};
+			$(this).parents().find(".block-toggle").css("background-size","60% 100%");
+    		
     	}
     	else{
     		$(this).parent('.button-read').siblings('.expander').find('.less-link').click();
     		$(this).text("Read more");
-    		if ($(window).width() >= 1900) {
-    			$(this).parents().find(".block-toggle").css("background-size","auto 100%");
-    		};
+			$(this).parents().find(".block-toggle").css("background-size","auto 100%");
+    		
     	}
     	$(this).toggleClass('more');
 	 	event.preventDefault();
